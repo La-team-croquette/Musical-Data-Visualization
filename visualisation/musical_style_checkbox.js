a7e3b478ec67c704517ff79cfdaad7e54a855c02
jkritikos <jeremy.kritikos@neuf.fr>
2021-12-14 14:42:29 +0100

    File created.

---
+++ visualisation/musical_style_checkbox.js
@@ -0,0 +1,10 @@
+
+dataMock =  {
+    "endTime": "2021-01-05 11:28",
+    "artistName": "Lorenzo",
+    "trackName": "Tu le C",
+    "msPlayed": 53393,
+    "genre":["Rock","Rap"],
+    "country":"France",
+    "listener":"Tom"
+}
