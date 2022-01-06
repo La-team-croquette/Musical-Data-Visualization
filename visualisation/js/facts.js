60da0fe130d0d0cb3c232c6b717ae06dddb12a8b
Tom <tom.montauriol@gmail.com>
2022-01-06 11:46:23 +0100

    Ajout facts 2.

--- visualisation/js/facts.js
+++ visualisation/js/facts.js
@@ -4,7 +4,12 @@ d3.json("../data/formated_json.json").then(function (data) {
     data.forEach(d => time_total += d.msPlayed)
     document.getElementById("fact_time_total").innerText = Math.round(time_total / 3600000).toString();
 
-    // let artists = {}
-    // data.forEach(d =>)
+    let artists = {}
+    data.forEach(d => artists[d.artistName] = 1)
+    document.getElementById("fact_nb_artists").innerText = Object.keys(artists).length.toString();
+
+    d3.json("../jsonToNodesLinks/nodes_links.json").then(function (links_nodes) {
+        document.getElementById("fact_nb_shared").innerText = links_nodes.nodes.length;
+    });
 
 });
