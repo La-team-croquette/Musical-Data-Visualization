efeeaab246854ae819c333546645fe7c71f3b1e4
Tom <tom.montauriol@gmail.com>
2022-01-06 11:20:02 +0100

    Ajout facts

---
+++ visualisation/js/facts.js
@@ -0,0 +1,10 @@
+d3.json("../data/formated_json.json").then(function (data) {
+    document.getElementById("fact_nb_songs").innerText = data.length;
+    let time_total = 0;
+    data.forEach(d => time_total += d.msPlayed)
+    document.getElementById("fact_time_total").innerText = Math.round(time_total / 3600000).toString();
+
+    // let artists = {}
+    // data.forEach(d =>)
+
+});
